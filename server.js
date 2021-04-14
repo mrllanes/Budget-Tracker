eb6506672d7f4d41ec2c5b3660d400088644e0f0
Mr. Llanes <llanesm@gmail.com>
2021-04-13 23:13:16 -0500

    Manifest, Service Worker and modifications to server and index

--- server.js
+++ server.js
@@ -15,14 +15,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+    useNewUrlParser: true,
+    useFindAndModify: false,
 });
 
 // routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+    console.log(`App running on port ${PORT}!`);
 });
